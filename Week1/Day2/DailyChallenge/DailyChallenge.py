8b0df699938352d1b0a6aa148937a5766c51b7f6
lizabguercho <ielyzaveta.rabkina@outlook.com>
2025-08-21 16:45:33 +0300

    finished DC day2

---
+++ Week1/Day2/DailyChallenge/DailyChallenge.py
@@ -0,0 +1,14 @@
+# Challenge_1
+number_input = int(input ("Type a number: "))
+length_input = int(input ("Type a length: "))
+
+multiple_list = []
+
+for num in range(1,length_input+1):
+    multiple_list.append(number_input*num)
+
+print(multiple_list)
+
+
+# Challenge_2
+
